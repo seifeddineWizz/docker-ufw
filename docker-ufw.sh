fa85b568244bb6eb1adfd8d224d4b3c55147276b
seifeddineWizz <167075846+seifeddineWizz@users.noreply.github.com>
2025-12-16 10:38:33 +0100

    Update docker-ufw.sh

--- docker-ufw.sh
+++ docker-ufw.sh
@@ -9,7 +9,7 @@ ALLOWED_IPS_FILE="/etc/iptables/allowed-ips.txt"
 
 iptables -F DOCKER-USER
 iptables -I DOCKER-USER -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A DOCKER-USER -i br+ -o br+ -j ACCEPT
+iptables -A DOCKER-USER -i docker0 -o docker0  -j ACCEPT
 iptables -A DOCKER-USER -j DROP
 
 while IFS= read -r ip; do
